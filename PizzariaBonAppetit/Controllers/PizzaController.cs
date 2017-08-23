beb38082babc8d81821edcc9a922a55c0ced2563
Thiago Andrey zils <thiago.zils@catolicasc.org.br>
2017-08-22 22:03:52 -0300

    Iniciado alterações.

--- PizzariaBonAppetit/Controllers/PizzaController.cs
+++ PizzariaBonAppetit/Controllers/PizzaController.cs
@@ -9,19 +9,21 @@ namespace PizzariaBonAppetit.Controllers
 {
     public class PizzaController : Controller
     {
+
+        public List<Pizza> Pizzas = new List<Pizza>
+        {
+            new Pizza {Name = "4Queijos", Id = 1 , Prize= 22.50, HaveEdge = true},
+            new Pizza {Name = "Bacon", Id = 2, Prize= 30.65, HaveEdge = true},
+            new Pizza {Name = "Calabresa", Id = 3, Prize= 12.2, HaveEdge = false},
+            new Pizza {Name = "Brocólis", Id = 4, Prize= 15.69, HaveEdge = true}
+        };
+
+
         // GET: Pizza
         public ActionResult Index()
         {
 
-            var pizza = new Pizza()
-            {
-                Id = 1,
-                Name = "4 Queijos",
-                Prize = 30.0,
-                HaveEdge = true
-
-            };
-            return View(pizza);
+            return View(Pizzas);
 
 
 
