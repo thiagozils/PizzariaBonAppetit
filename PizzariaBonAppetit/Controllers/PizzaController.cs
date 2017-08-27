ab4eb3e6c87454a813ef545360c83f8fcafe3eea
Thiago Andrey zils <thiago.zils@catolicasc.org.br>
2017-08-27 11:50:03 -0300

    Atividade - Implementar Tabelas bootstrap e tela de detalhes em seu projeto

--- PizzariaBonAppetit/Controllers/PizzaController.cs
+++ PizzariaBonAppetit/Controllers/PizzaController.cs
@@ -4,29 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PizzariaBonAppetit.Models;
+using PizzariaBonAppetit.ViewModels;
 
 namespace PizzariaBonAppetit.Controllers
 {
     public class PizzaController : Controller
     {
-
-        public List<Pizza> Pizzas = new List<Pizza>
-        {
-            new Pizza {Name = "4Queijos", Id = 1 , Prize= 22.50, HaveEdge = true},
-            new Pizza {Name = "Bacon", Id = 2, Prize= 30.65, HaveEdge = true},
-            new Pizza {Name = "Calabresa", Id = 3, Prize= 12.2, HaveEdge = false},
-            new Pizza {Name = "Brocólis", Id = 4, Prize= 15.69, HaveEdge = true}
-        };
-
-
         // GET: Pizza
         public ActionResult Index()
         {
 
-            return View(Pizzas);
+            var pizzas = new List<Pizza>
+            {
+                new Pizza {Name = "4Queijos", Id = 1 , Prize= 22.50, HaveEdge = true},
+                new Pizza {Name = "Bacon", Id = 2, Prize= 30.65, HaveEdge = true},
+                new Pizza {Name = "Calabresa", Id = 3, Prize= 12.2, HaveEdge = false},
+                new Pizza {Name = "Brocólis", Id = 4, Prize= 15.69, HaveEdge = true}
 
+            };
 
+            var viewModel = new PizzaIndexViewModel
+            {
+                Pizzas = pizzas
+            };
 
-        }
+            return View(viewModel);
+       }
     }
 }
